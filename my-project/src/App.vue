a2f8fdf3b934d4319047389bbb83e0a2b03439c2
Luis Vásquez <59650086+DonPasticho@users.noreply.github.com>
2022-09-30 22:29:51 -0430

    Project completed.

---
+++ my-project/src/App.vue
@@ -0,0 +1,406 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="border-b border-gray-100 drop-shadow-sm
+  sticky top-0 bg-white z-10">
+    <div class="max-w-screen-xl mx-auto py-5 px-5 md:px-0">
+      <div class="flex items-center justify-between">
+        <img src="./assets/logo.png" class="w-56" alt="logo">
+        <div class="hidden md:block">
+          <ul class="flex items-center space-x-5 
+          text-gray-500 font-semibold">
+            <li><a href="">Authors</a></li>
+            <li><a href="">Articles</a></li>
+            <li><a href="">About</a></li>
+          </ul>
+        </div>
+        <div>
+          <button class="bg-indigo-600 text-white rounded-lg 
+          px-4 py-2">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <section class="bg-gray-50 px-5 py-10 md:py-32">
+    <div class="max-w-screen-xl mx-auto">
+      <div class="group cursor-pointer
+      grid grid-cols-1 md:grid-cols-2
+      gap-y-3 gap-x-20 md:gap-y-4 
+      items-center">
+        <div>
+          <div class="overflow-hidden rounded-lg drop-shadow-lg">
+            <img src="./assets/iPhone.jpg" alt="" class="duration-300
+            ease-in group-hover:scale-110">
+            <div class="absolute top-0 left-0 bottom-0 right-0
+            bg-gray-900 opacity-10 group-hover:opacity-60
+            duration-800 transition ease-in">
+            </div>
+          </div>
+        </div>
+        <a href="#" class="space-y-3 text-sm">
+          <div class="flex items-center text-sm">
+            <div class="text-indigo-600">
+              Management
+            </div>
+            <div class="">
+              &nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            <div class="">
+              5 min read
+            </div>
+          </div>
+          <h2 class="text-2xl md:text-4xl text-gray-900
+          font-semibold">
+            Why Slack is Problematic and Ruining Depp Work for Staff
+          </h2>
+          <p class="text-sm md:text-lg text-gray-600">
+            Slack seemed like the saving grace to fix all email
+            trouble, but instead of replacing email, it has only
+            become another tool for distraction.
+          </p>
+          <div class="text-lg text-gray-600 flex items-center 
+          space-x-2">
+            <div>Read article</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" 
+            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+            </svg>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="max-w-screen-xl mx-auto px-5 py-10 md:py-20">
+    <h1 class="text-3xl font-medium text-gray-800 mb-10">
+      Lastest blog posts
+    </h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 gap-y-20">
+      <div>
+        <a href="#" class="group space-y-3">
+          <div class="overflow-hidden rounded-lg drop-shadow-lg">
+            <img src="./assets/landscape1.jpg" alt="" class="duration-300 ease-in 
+            group-hover:sacle-110">
+            <div class="absolute top-0 left-0 bottom-0 right-0 
+            bg-gray-900 opacity-10 group-hover:opacity-60 
+            duration-800 transition ease-in">
+            </div>
+          </div>
+          <div class="flex items-center text-sm">
+            <div class="text-indigo-600">Management</div>
+            <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div>5 min</div>
+          </div>
+          <h2 class="text-2xl md:text-3xl text-gray-800 font-medium">
+
+          </h2>
+          <p class="text-sm md:text-base text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit provident aliquid quidem laudantium deserunt, 
+            quasi et non deleniti necessitatibus. 
+            Quasi, iure consequatur? Aliquam veritatis cupiditate 
+            similique reiciendis exercitationem, at tempora.
+          </p>
+        </a>
+      </div>
+      <div>
+        <a href="#" class="group space-y-3">
+          <div class="overflow-hidden rounded-lg drop-shadow-lg">
+            <img src="./assets/landscape1.jpg" alt="" class="duration-300 ease-in 
+            group-hover:sacle-110">
+            <div class="absolute top-0 left-0 bottom-0 right-0 
+            bg-gray-900 opacity-10 group-hover:opacity-60 
+            duration-800 transition ease-in">
+            </div>
+          </div>
+          <div class="flex items-center text-sm">
+            <div class="text-indigo-600">Management</div>
+            <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div>5 min</div>
+          </div>
+          <h2 class="text-2xl md:text-3xl text-gray-800 font-medium">
+
+          </h2>
+          <p class="text-sm md:text-base text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit provident aliquid quidem laudantium deserunt, 
+            quasi et non deleniti necessitatibus. 
+            Quasi, iure consequatur? Aliquam veritatis cupiditate 
+            similique reiciendis exercitationem, at tempora.
+          </p>
+        </a>
+      </div>
+      <div>
+        <a href="#" class="group space-y-3">
+          <div class="overflow-hidden rounded-lg drop-shadow-lg">
+            <img src="./assets/landscape1.jpg" alt="" class="duration-300 ease-in 
+            group-hover:sacle-110">
+            <div class="absolute top-0 left-0 bottom-0 right-0 
+            bg-gray-900 opacity-10 group-hover:opacity-60 
+            duration-800 transition ease-in">
+            </div>
+          </div>
+          <div class="flex items-center text-sm">
+            <div class="text-indigo-600">Management</div>
+            <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+            <div>5 min</div>
+          </div>
+          <h2 class="text-2xl md:text-3xl text-gray-800 font-medium">
+
+          </h2>
+          <p class="text-sm md:text-base text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugit provident aliquid quidem laudantium deserunt, 
+            quasi et non deleniti necessitatibus. 
+            Quasi, iure consequatur? Aliquam veritatis cupiditate 
+            similique reiciendis exercitationem, at tempora.
+          </p>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="relative bg-gray-800 py-20 drop-shadow-lg px-5
+  md:px-0">
+    <div class="max-w-screen-xl m-auto">
+      <div class="text-center text-white space-y-10">
+        <h2 class="text-2xl md:text-4xl font-bold">
+          Get our weekly email
+        </h2>
+        <p class="text-sm md:text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Fugit provident aliquid quidem laudantium deserunt, 
+          quasi et non deleniti necessitatibus. 
+          Quasi, iure consequatur? Aliquam veritatis cupiditate 
+          similique reiciendis exercitationem, at tempora.
+        </p>
+        <div class="email-sign-up padding-top w-form">
+          <form action="" class="m-4 flex items-center
+          justify-center">
+            <input type="text" class="shadow rounded-l-lg
+            p-4 border-t mr-0 border-b border-l text-gray-800
+            border-gray-200 bg-white" placeholder="your@mail.com">
+            <button class="shadow px-8 rounded-r-lg 
+            bg-gray-900 border-gray-900 text-white
+            font-bold p-4 uppercase border-t border-0
+            ">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="max-w-screen-xl mx-auto py-20 px-5 md:px-0">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-x-20
+    space-y-10 md:space-y-0">
+      <div>
+        <div>
+          <a href="#" class="inline-block">
+            <img src="./assets/landscape1.jpg" alt="" class="rounded-lg">
+            <div class="space-y-5 py-5">
+              <div class="flex items-center text-sm">
+                <div class="text-indigo-600">Management</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                <div>5 min read</div>
+              </div>
+              <h3 class="text-xl md:text-3xl font-bold">
+                7 of the Best Examples of Beautiful Blog Design
+              </h3>
+              <p class="text-sm md:text-xl text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Fugit provident aliquid quidem laudantium deserunt, 
+                quasi et non deleniti necessitatibus. 
+                Quasi, iure consequatur? Aliquam veritatis cupiditate 
+                similique reiciendis exercitationem, at tempora.
+              </p>
+              <div class="flex items-center text-gray-500
+              uppercase text-sm space-x-2">
+                <div class="uppercase">james corvett</div>
+                <div class="spacer-dot">.</div>
+                <div>SEP 20, 2022</div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div>
+        <h4 class="font-bold text-lg mb-2">Featured</h4>
+        <div class="mt-5">
+          <div>
+            <div>
+              <a href="#" class="grid grid-cols-2">
+                <div class="h-full flex-1 flex flex-col 
+                justify-center space-y-2 mr-10">
+                  <div class="uppercase text-indigo-600 font-semibold">
+                    Categories
+                  </div>
+                  <h4 class="font-semibold text-gray-900 text-lg">
+                    Title
+                  </h4>
+                  <p class="text-sm text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fugit provident aliquid quidem laudantium deserunt, 
+                    quasi et non deleniti necessitatibus. 
+                    Quasi, iure consequatur? Aliquam veritatis cupiditate 
+                    similique reiciendis exercitationem, at tempora.
+                  </p>
+                  <div class="flex items-center text-gray-500 text-sm
+                  space-x-1">
+                    <div>Read more</div>
+                    
+                  </div>
+                </div>
+                <img src="./assets/landscape1.jpg" alt="" class="rounded-lg">
+              </a>        
+            </div>
+          </div> 
+        </div>
+        <div class="mt-5">
+          <div>
+            <div>
+              <a href="#" class="grid grid-cols-2">
+                <div class="h-full flex-1 flex flex-col 
+                justify-center space-y-2 mr-10">
+                  <div class="uppercase text-indigo-600 font-semibold">
+                    Categories
+                  </div>
+                  <h4 class="font-semibold text-gray-900 text-lg">
+                    Title
+                  </h4>
+                  <p class="text-sm text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fugit provident aliquid quidem laudantium deserunt, 
+                    quasi et non deleniti necessitatibus. 
+                    Quasi, iure consequatur? Aliquam veritatis cupiditate 
+                    similique reiciendis exercitationem, at tempora.
+                  </p>
+                  <div class="flex items-center text-gray-500 text-sm
+                  space-x-1">
+                    <div>Read more</div>
+                    
+                  </div>
+                </div>
+                <img src="./assets/landscape1.jpg" alt="" class="rounded-lg">
+              </a>        
+            </div>
+          </div> 
+        </div>
+        <div class="mt-5">
+          <div>
+            <div>
+              <a href="#" class="grid grid-cols-2">
+                <div class="h-full flex-1 flex flex-col 
+                justify-center space-y-2 mr-10">
+                  <div class="uppercase text-indigo-600 font-semibold">
+                    Categories
+                  </div>
+                  <h4 class="font-semibold text-gray-900 text-lg">
+                    Title
+                  </h4>
+                  <p class="text-sm text-gray-600">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Fugit provident aliquid quidem laudantium deserunt, 
+                    quasi et non deleniti necessitatibus. 
+                    Quasi, iure consequatur? Aliquam veritatis cupiditate 
+                    similique reiciendis exercitationem, at tempora.
+                  </p>
+                  <div class="flex items-center text-gray-500 text-sm
+                  space-x-1">
+                    <div>Read more</div>
+                    
+                  </div>
+                </div>
+                <img src="./assets/landscape1.jpg" alt="" class="rounded-lg">
+              </a>        
+            </div>
+          </div> 
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="bg-gray-50 border-t border-gray-200 px-4">
+    <div class="max-w-screen-xl mx-auto py-5 md:py-10">
+      <div class="grid grid-cols-1 md:grid-cols-12 py-5 
+      space-y-10 md:space-y-0">
+        <div class="md:col-span-4 mx-auto md:mx-0">
+          <a href="#" class="mb-5">
+            <img src="./assets/logo.png" alt="logo" width="212">
+          </a>
+          <p class="text-gray-400 mt-5 text-center md:text-left">
+            Bring your ideas alive.
+            <br>
+            Feel the Sunshine.
+            <br>
+          </p>
+        </div>
+        <div class="md:col-span-4 grid grid-cols-2">
+
+          <div class="flex flex-col space-y-2 mx-auto">
+            <h5 class="text-indigo-600 font-bold mb-2">Blog</h5>
+            <a href="#" class="text-gray-800">All Posts</a>
+            <a href="#" class="text-gray-800">About</a>
+            <a href="#" class="text-gray-800">Contact</a>
+            <a href="#" class="text-gray-800">Terms &amp; Conditions</a>
+            <a href="#" class="text-gray-800">Privacy Policy</a>
+          </div>
+          <div class="flex flex-col space-y-2 mx-auto">
+            <h5 class="text-indigo-600 font-bold mb-2">Admin</h5>
+            <a href="#" class="text-gray-800">Style Guide</a>
+            <a href="#" class="text-gray-800">Licenses</a>
+            <a href="#" class="text-gray-800">Instructions</a>
+            <a href="#" class="text-gray-800">Password</a>
+            <a href="#" class="text-gray-800">404</a>
+          </div>
+        </div>
+    
+        <div class="md:col-span-4 text-center md:text-left">
+          <h5 class="text-indigo-600 font-bold mb-2">Subscribe</h5>
+          <p class="text-gray-800 mb-5">
+            Sign up for the frequemcu Blog. <br> Get the latest news,
+            company insights, and frequency updates.
+          </p>
+
+          <div class="bg-white rounded-lg border border-gray-300">
+            <div class="flex flex-wrap justify-between md:flex-row">
+              <input type="email" class="m-1 p-3 md:w-80
+              appearance-none border-none text-gray-700 text-sm
+              font-medium focus:outline-none focus:border-white
+              focus:rounded focus:placeholder-transparent"
+              placeholder="Enter your email"
+              aia-label="Enter your email">
+              <button class="w-full text-sm m-1 p-2 bg-gray-800
+              rounded-lg font-semibold lg:w-auto hover:bg-gray-700
+              text-white">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full border-b border-gray-100 my-5"></div>
+      <div class="grid grid-cols-1 md:grid-cols-2 py-5">
+        <div class="hidden md:block md:col-span-7 text-gray-600">
+          ☺ All Rights Reserved.
+        </div>
+        <div class="md:col-span-5">
+          <div class="flex items-center md:justify-end">
+            <div class="text-gray-600 mr-2">Follow Us:</div>
+            <div class="flex space-between items-center space-x-2">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
